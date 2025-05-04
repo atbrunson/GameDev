1b97360a902aef2b41b2132a30aa12b7591eab43
PressureSwitch <atbrunson@gmail.com>
2025-05-03 20:09:50 -0500

    Organized / Perlin Noise Demo

---
+++ 02 TerrianGen/lib/helpers.js
@@ -0,0 +1,6 @@
+function vNorm(vectorA, vectorB){
+	this.vecA = vectorA;
+	this.vecB = vectorB;
+
+	return cross(this.vecA,this.vecB);
+}
--- gameDev/Physics/helpers.js
+++
@@ -1,7 +0,0 @@
-vNorm(vectorA, vectorB){
-	this.vecA = vectorA
-	this.vecB = vectorB
-
-	return cross(this.vecA,this.vecB)
-	
-}
