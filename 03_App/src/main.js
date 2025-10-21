7597ef15e73174c2b244fde690077abc362f0489
Andrew Brunson <atbrunson@gmail.com>
2025-10-21 16:22:14 -0500

    softbag added

--- 03_App/src/main.js
+++ 03_App/src/main.js
@@ -28,22 +28,23 @@ const engine = Engine.create();
 const world = engine.world;
 document.engine = engine; //---For Debugging Only---//
 export { engine };
+// Set properties of the WORLD
+engine.gravity.scale = 0.0;
 
 // Import User-Defined Modules
 import { loadSvg, loadSvgPaths, select } from "./loadSVG.js";
 import { Ship } from "./ship.js";
 import { ProgressBar } from "./ui/progress_bar.js";
 import { Drill } from "./drill.js";
-import { SoftBody } from "./soft_body.js";
+import { SoftBody, SoftBag } from "./soft_body.js";
 import { SVGcomposite } from "./SvgComposite.js";
-// Set properties of the WORLD
-engine.gravity.scale = 0.0;
 
 // Create & start the RUNNER
 var runner = Runner.create();
 Runner.run(runner, engine);
 
-// Create & start the RENDERER (window.document.render)
+/** Create & start the RENDERER (window.document.render)
+ */
 const render = Render.create({
   element: document.body,
   engine: engine,
@@ -55,36 +56,43 @@ const render = Render.create({
     showAngleIndicator: false,
     showCollisions: false,
     showVelocity: false,
-    showDebug: false,
+    showDebug: true,
     showInternalEdges: true,
     showBounds: true,
-    wireframes: false,
+    wireframes: true,
+    showIds: true,
   },
 });
 Render.run(render);
+
+
 console.log("render", render);
 document.render = render; //---For Debugging Only---//
-export { render };
+export { render }; // to be able to access render in user-defined modules
+
+
+const myBaggie = new SoftBag(100, 100, 5, 4, 10, true, 10);
+
 
 // Create composite for our container
-// const container = Composite.create({
-//   bodies: [
-//     // Specifies four rectangles for the walls floor & ceiling
-//     Bodies.rectangle(400, -25, 850, 50, { isStatic: true, label: "Ceiling" }),
-//     Bodies.rectangle(400, 625, 850, 50, { isStatic: true, label: "Floor" }),
-//     Bodies.rectangle(825, 300, 50, 700, {
-//       isStatic: true,
-//       label: "Right Wall",
-//     }),
-//     Bodies.rectangle(-25, 300, 50, 700, { isStatic: true, label: "Left Wall" }),
-//   ],
-//   label: "Container",
-// });
-// Composite.add(world, container);
+const container = Composite.create({
+  bodies: [
+    // Specifies four rectangles for the walls floor & ceiling
+    Bodies.rectangle(400, -25, 850, 50, { isStatic: true, label: "Ceiling" }),
+    Bodies.rectangle(400, 625, 850, 50, { isStatic: true, label: "Floor" }),
+    Bodies.rectangle(825, 300, 50, 700, {
+      isStatic: true,
+      label: "Right Wall",
+    }),
+    Bodies.rectangle(-25, 300, 50, 700, { isStatic: true, label: "Left Wall" }),
+  ],
+  label: "Container",
+});
+Composite.add(world, container);
 
-// // Create SVG Composite
-// const bodySVG = await loadSvgPaths("./hollow.svg");
-// console.log("bodySVG", bodySVG);
+// Create SVG Composite
+const bodySVG = await loadSvgPaths("./hollow.svg");
+console.log("bodySVG", bodySVG);
 
 // // convert the path to matter.js vertices
 // bodySVG.vertices = bodySVG.map((path) => Svg.pathToVertices(path, 50));
@@ -121,25 +129,41 @@ export { render };
 // load an SVG from a URL ---CAVE TERRIAN---
 // const bodySVG= await loadSvgPaths("./hollow.svg");
 
-/**
- * Create SVG Composite
+/** Create SVG Composite
+ *  @todo load path styles from SVG
+ *  @todo render path styles from SVG
  */
-const mySVG = await loadSvgPaths("./hollow.svg");
-console.log("mySVG", mySVG);
-const MyCave = new SVGcomposite(mySVG, 5, 450, 500);
-console.log("MyCave", MyCave);
-
-
-//LOADED PATH STYLES
-mySVG.styles = mySVG.map((path) => {return path.getAttribute("style")})
-const newSVG = await loadSvg("./hollow.svg");
-const paths = newSVG.querySelectorAll("path")
-console.log("newSVG paths", paths);
-const style = paths[0].getAttribute("style");
-console.log("newSVG style", style);
-
-
-// Create MOUSE Object and MouseConstraint
+// const mySVG = await loadSvgPaths("./hollow.svg");
+// console.log("mySVG", mySVG);
+// const MyCave = new SVGcomposite(mySVG, 5, 450, 500);
+// console.log("MyCave", MyCave);
+//LOAD SVG PATH STYLES
+// mySVG.styles = mySVG.map((path) => {return path.getAttribute("style")})
+// const newSVG = await loadSvg("./hollow.svg");
+// const paths = newSVG.querySelectorAll("path")
+// console.log("newSVG paths", paths);
+// const style = paths[0].getAttribute("style");
+// console.log("newSVG style", style);
+  
+/** Create_SHIP_object
+ */
+const ship = new Ship(700, 450, 15);
+ship.body.label = "ship";
+ship.fuel = 0.75;
+// debugging only
+document.ship = ship;
+  
+/** Create_DRILL_object
+ */
+const drill = new Drill(0, 0, 7.5);
+  
+/** Create_INPUT_CONTROLS
+ * @todo Move to new input_controls.js
+ * @todo Combine keyboard_controls.js and new input_controls.js file
+ * @todo Create Mouse Control Class in src/input_controls.js
+ * @todo Create view controls with every maouse or keyboard control
+ */
+// Create MOUSE Object and MouseConstraint 
 const mouse = Mouse.create(render.canvas),
   mouseConstraint = MouseConstraint.create(engine, {
     mouse: mouse,
@@ -150,31 +174,9 @@ const mouse = Mouse.create(render.canvas),
       },
     },
   });
-
 Composite.add(world, mouseConstraint);
 // keep the mouse in sync with rendering
 render.mouse = mouse;
-
-//---Create_PLAYER_object---//
-// import { Player } from "./lib/player.js";
-// var player0 = new Player(400, 300, 25);
-// player0.body.label = 'player0';
-
-//---Create_SHIP_object---//
-const ship = new Ship(700, 450, 15);
-ship.body.label = "ship";
-ship.fuel = 0.75;
-// debugging only
-document.ship = ship;
-
-//---Create_DRILL_object---//
-const drill = new Drill(0, 0, 7.5);
-
-// TODO:
-// - Create Mouse Control Class in src/input_controls.js
-// - Combine keyboard_controls.js and new input_controls.js file
-// - Commbine view_controls.js with new input_controls.js file
-// - Create view controls with every maouse or keyboard control
 
 // mouse down right button starts drilling
 render.canvas.addEventListener("mousedown", function (e) {
@@ -185,11 +187,18 @@ render.canvas.addEventListener("mouseup", function (e) {
   e.button === 2 ? drill.stopDrilling() : null;
 });
 
-//---Create_Progress_Bar---//
+
+/** Create_Progress_Bar
+ * @todo move to new ui_elements.js
+ * @todo move and scale ui elements with view controls (or create new canvas for UI elements)
+ */
 window.progbar1 = new ProgressBar(5, 300, ship, "ship.fuel", 0, 1);
 console.log("progBar1", progbar1);
 
-//---MAIN_GAME_LOOP---//
+/** MAIN GAME LOOP 
+ * 
+ * 
+*/
 // Events.on(engine, "beforeUpdate", function () { //Everything below this will run before every engine update
 // })
 
@@ -239,8 +248,14 @@ Events.on(render, "afterRender", function () {
     );
   }
 });
-//---END_MAIN_GAME_LOOP---//
+/** END MAIN GAME LOOP */
 
+
+
+/** HTML manipulation, formatting, and styling
+ * 
+ * 
+ */
 // Add html dom element with instructions & allign left with canvas
 var instructions = document.createElement("div");
 instructions.setAttribute("id", "instructions");
--- 03_App/src/soft_body.js
+++ 03_App/src/soft_body.js
@@ -1,4 +1,4 @@
-import Matter from "matter-js";
+import Matter, { Body } from "matter-js";
 import { engine, render } from "./main.js";
 import { KeyboardControl } from "./keyboard_control.js";
 
@@ -43,10 +43,15 @@ class SoftBody {
     const Composite = Matter.Composite;
     const Composites = Matter.Composites;
     const Bodies = Matter.Bodies;
+    const World = Matter.World;
+    const Body = Matter.Body;
 
     particleOptions = Common.extend({ inertia: Infinity }, particleOptions);
     constraintOptions = Common.extend(
-      { stiffness: 0.2, render: { type: "line", anchors: false } },
+      {
+        stiffness: 0.2,
+        render: { strokeStyle: "stroke-opacity: 50%;", type: "line", anchors: false },
+      },
       constraintOptions
     );
 
@@ -73,4 +78,98 @@ class SoftBody {
   }
 }
 
-export { SoftBody };
+class SoftBag {
+  /**
+   * Creates a soft body with a 60° pitch pattern connected to each other.
+   * @class SoftBody
+   * @param {number} xx position
+   * @param {number} yy position
+   * @param {number} columns
+   * @param {number} rows
+   * @param {number} gap
+   * @param {boolean} crossBrace
+   * @param {number} particleRadius
+   * @param {} particleOptions
+   * @param {} constraintOptions
+   */
+  constructor(
+    xx,
+    yy,
+    rows,
+    columns,
+    gap,
+    crossBrace,
+    particleRadius,
+    particleOptions,
+    constraintOptions
+  ) {
+    this.xx = xx;
+    this.yy = yy;
+    this.rows = rows;
+    this.columns = columns * 2 - 1;
+    this.particleRadius = particleRadius;
+    this.gap = gap;
+    this.crossBrace = crossBrace;
+    this.label = "SoftBag";
+
+    const Common = Matter.Common;
+    const Composite = Matter.Composite;
+    const Composites = Matter.Composites;
+    const Bodies = Matter.Bodies;
+
+    particleOptions = Common.extend({ inertia: Infinity }, particleOptions);
+    constraintOptions = Common.extend(
+      { stiffness: 0.2, render: { type: "line", anchors: false } },
+      constraintOptions
+    );
+
+    this.matter = Composites.stack(
+      this.xx,
+      this.yy,
+      this.columns,
+      this.rows,
+      this.gap,
+      this.gap,
+      function (x, y) {
+        return Bodies.circle(x, y, this.particleRadius, particleOptions);
+      }.bind(this)
+    );
+
+    Composites.mesh(
+      this.matter,
+      this.columns,
+      this.rows,
+      this.crossBrace,
+      constraintOptions
+    );
+
+    this.matter.bodies = this.matter.bodies.filter((body, index) => index % 2 === 0);
+
+    console.log(this.matter);
+
+    // Add the 60° pitch pattern
+    this.matter.constraints = this.matter.constraints.filter(
+      // keeps if retuns true
+      (constraint) =>
+        constraint.bodyB.id !== this.columns + constraint.bodyA.id && // keep if bodyB.id is not equal to columns + bodyA.id
+        constraint.bodyB.id !== 1 + constraint.bodyA.id && // keep if bodyB.id is not equal to 1 + bodyA.id
+        constraint.bodyB.id % 2 === 0 // keep if bodyB.id is even
+    );
+
+    const topbodies = this.matter.bodies.filter(
+      (body, index) => index % this.rows === 0
+    );
+    console.log("topbodies", topbodies);
+
+    Composites.chain(this.matter, 0, 0, 0, 0, {
+      stiffness: 0.2,
+      render: { type: "line", anchors: false },
+    });
+    // add contraints for the remaining bodies
+
+    Composite.add(engine.world, this.matter);
+    console.log(`${this.label}`, this.matter);
+  }
+}
+
+export { SoftBody, SoftBag };
