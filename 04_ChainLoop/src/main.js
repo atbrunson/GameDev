a4395fe08afc6b563c2fcaa3a5fb06a939960ca9
PressureSwitch <atbrunson@gmail.com>
2026-02-01 21:20:45 -0600

    attempts at SVG and npm clean up

--- 04_ChainLoop/src/main.js
+++ 04_ChainLoop/src/main.js
@@ -1,5 +1,7 @@
 //matter.js testing
 
+console.log(app);
+
 // module aliases
 const Engine = Matter.Engine,
   Render = Matter.Render,
@@ -11,7 +13,10 @@ const Engine = Matter.Engine,
   MouseConstraint = Matter.MouseConstraint,
   Mouse = Matter.Mouse,
   Bodies = Matter.Bodies;
-  Common = Matter.Common;
+  Events = Matter.Events;
+Common = Matter.Common;
+
+
 
 // create engine
 let engine = Engine.create(),
@@ -22,19 +27,33 @@ world.gravity.scale = 0.0;
 
 // create renderer
 let render = Render.create({
-  element: document.body,
+  element: document.getElementById("app"),
   engine: engine,
   options: {
     width: 1000,
-    height: 1000,
+    height: 600,
     wireframes: false,
     showAngleIndicator: true,
     showCollisions: false,
     showVelocity: true,
-    showDebug: true,
-		showIds: false
+    showDebug: false,
+    showIds: false,
   },
 });
+console.log(render);
+// create svg container on top of the canvas
+let draw = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+draw.setAttribute("id", "draw")
+draw.setAttribute("width", render.options.width);
+draw.setAttribute("height", render.options.height);
+draw.setAttribute("viewBox", "0 0 " + render.options.width + " " + render.options.height);
+
+document.getElementById("ui").appendChild(draw)
+
+console.log(draw);
+var meta= SVG().nested(draw)
+var rect = SVG().rect(100, 100).fill('red')
+draw.set
 
 // fit the render viewport to the scene
 Render.lookAt(render, {
@@ -64,7 +83,6 @@ let mouse = Mouse.create(render.canvas),
   });
 
 Composite.add(world, mouseConstraint);
-
 
 // keep the mouse in sync with rendering
 render.mouse = mouse;
@@ -96,7 +114,7 @@ Composite.add(box, leftWall);
 Composite.add(box, rightWall);
 Composite.add(box, ceiling);
 
-// generates ring of bodies and chaning them together
+// generates ring of bodies and chaining them together
 // TODO:
 // - separate loop chaining from ring creation
 // - create ring function
@@ -105,10 +123,10 @@ Composite.add(box, ceiling);
 let nBodies = 100;
 let linkDia = 5;
 let linkMass = 1;
-let cLength =  0.50
+let cLength = 0.5;
 let cDamp = 1;
-let cStiff = .00001;
-let displayLink = false
+let cStiff = 0.00001;
+let displayLink = false;
 
 // create ring of bodies
 let ringDia = (nBodies * linkDia + (nBodies - 1) * cLength) / Math.PI;
@@ -151,14 +169,22 @@ Composite.add(
       type: "line",
       visible: displayLink,
     },
-  })
+  }),
 );
 
-
 ring.bodies.forEach((b) => {
-	Body.rotate(b, Math.PI/2 + ( ringAngle ) * ring.bodies.indexOf(b))
-	console.log("body " + b.id, 45 + ringAngle * ring.bodies.indexOf(b));
-})
-
+  Body.rotate(b, Math.PI / 2 + ringAngle * ring.bodies.indexOf(b));
+  console.log("body " + b.id, 45 + ringAngle * ring.bodies.indexOf(b));
+});
 
 Composite.add(world, [box, ring]);
+
+
+
+
+
+
+function smin(a, b, k) {
+  let res = Math.exp(-k * a) + Math.exp(-k * b);
+  return -Math.log(res) / k;
+}
